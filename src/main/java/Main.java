ab7279e3e273cc8a13d324de8b59f81833560d0f
Soham Das <publicsoham.24@gmail.com>
2024-05-15 09:56:21 +0530

    pass stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -17,6 +19,28 @@ public class Main {
        serverSocket.setReuseAddress(true);
        clientSocket = serverSocket.accept(); // Wait for connection from client.
        System.out.println("accepted new connection");
+
+       String responseBody = "Hello, world!";
+
+       // Get output stream of client socket
+         OutputStream outputStream = clientSocket.getOutputStream();
+         PrintWriter out = new PrintWriter(outputStream, true);
+
+         // Send http response to client
+         out.print("HTTP/1.1 200 OK\r\n");
+         out.print("Content-Type: text/plain\r\n");
+         out.print("Content-Length: " + responseBody.length() + "\r\n");
+         out.print("\r\n");
+         out.print(responseBody);
+
+         // Ensure all data is sent by flushing the stream
+         out.flush();
+
+         // Close streams and sockets
+         out.close();
+         clientSocket.close();
+         serverSocket.close();
+
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      }
